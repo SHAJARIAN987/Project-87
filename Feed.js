4d28fa612e3dd8ade246e4ae1e745c10bb149681
SHAJARIAN987 <88212606+SHAJARIAN987@users.noreply.github.com>
2022-12-11 12:31:02 -0500

    Add files via upload

---
+++ Feed.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default class Feed extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>Feed!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  header: {},
+});
